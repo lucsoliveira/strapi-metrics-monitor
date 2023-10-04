16c4330187a915c3de2e91435deadc32a36afac7
Lucas Oliveira <olucas1998@gmail.com>
2023-10-04 13:29:16 -0300

    fix: put request inside try catch

--- src/index.ts
+++ src/index.ts
@@ -5,71 +5,91 @@ import { Table } from "console-table-printer";
 const projectName = process.argv[2] || "N/A";
 //@ts-ignore
 const urlMetrics = process.argv[3];
+//@ts-ignore
+const secondsUpdate = process.argv[4] || 2000;
 
 const convertBytesToMB = (value: number) => {
   return (value / (1024 * 1024)).toFixed(2) + " MB";
 };
 
 function getMetrics(url: string) {
-  const result = axios.get(url).then((res) => {
-    const { data } = res;
-    const lines = data.split("\n");
+  const result = axios
+    .get(url)
+    .then((res) => {
+      const { data } = res;
+      const lines = data.split("\n");
 
-    const aux = [];
+      let strapi_process_resident_memory_bytes = 0;
+      let strapi_process_virtual_memory_bytes = 0;
+      let strapi_nodejs_external_memory_bytes = 0;
+      let strapi_nodejs_heap_size_total_bytes = 0;
+      let strapi_nodejs_heap_size_used_bytes = 0;
 
-    let strapi_process_resident_memory_bytes = 0;
-    let strapi_process_virtual_memory_bytes = 0;
-    let strapi_nodejs_external_memory_bytes = 0;
-    for (const item of lines) {
-      const objAux = item.split(" ");
+      for (const item of lines) {
+        const objAux = item.split(" ");
 
-      if (objAux[0] === "strapi_process_resident_memory_bytes") {
-        strapi_process_resident_memory_bytes = Number(objAux[1]);
+        if (objAux[0] === "strapi_process_resident_memory_bytes") {
+          strapi_process_resident_memory_bytes = Number(objAux[1]);
+        }
+
+        if (objAux[0] === "strapi_process_virtual_memory_bytes") {
+          strapi_process_virtual_memory_bytes = Number(objAux[1]);
+        }
+
+        if (objAux[0] === "strapi_nodejs_heap_size_total_bytes") {
+          strapi_nodejs_heap_size_total_bytes = Number(objAux[1]);
+        }
+
+        if (objAux[0] === "strapi_nodejs_external_memory_bytes") {
+          strapi_nodejs_external_memory_bytes = Number(objAux[1]);
+        }
+
+        if (objAux[0] === "strapi_nodejs_heap_size_used_bytes") {
+          strapi_nodejs_heap_size_used_bytes = Number(objAux[1]);
+        }
       }
 
-      if (objAux[0] === "strapi_process_virtual_memory_bytes") {
-        strapi_process_virtual_memory_bytes = Number(objAux[1]);
-      }
+      const p = new Table();
 
-      if (objAux[0] === "strapi_nodejs_external_memory_bytes") {
-        strapi_nodejs_external_memory_bytes = Number(objAux[1]);
-      }
-    }
+      p.addRow({
+        index: 1,
+        text: "strapi_process_resident_memory_bytes",
+        value: convertBytesToMB(strapi_process_resident_memory_bytes),
+      });
+      p.addRow({
+        index: 2,
+        text: "strapi_process_virtual_memory_bytes",
+        value: convertBytesToMB(strapi_process_virtual_memory_bytes),
+      });
+      p.addRows([
+        {
+          index: 3,
+          text: "strapi_nodejs_external_memory_bytes",
+          value: convertBytesToMB(strapi_nodejs_external_memory_bytes),
+        },
+      ]);
+      p.addRows([
+        {
+          index: 4,
+          text: "strapi_nodejs_heap_size_total_bytes",
+          value: convertBytesToMB(strapi_nodejs_heap_size_total_bytes),
+        },
+      ]);
+      p.addRows([
+        {
+          index: 5,
+          text: "strapi_nodejs_heap_size_used_bytes",
+          value: convertBytesToMB(strapi_nodejs_heap_size_used_bytes),
+        },
+      ]);
 
-    const finalObj = {
-      strapiProcessResidentMemory: convertBytesToMB(
-        strapi_process_resident_memory_bytes
-      ),
-      strapiProcessVirtualMemoryBytes: convertBytesToMB(
-        strapi_process_virtual_memory_bytes
-      ),
-      strapiNodejsExternalMemoryBytes: convertBytesToMB(
-        strapi_nodejs_external_memory_bytes
-      ),
-    };
-
-    const p = new Table();
-
-    p.addRow({
-      index: 1,
-      text: "strapi_process_resident_memory_bytes",
-      value: convertBytesToMB(strapi_process_resident_memory_bytes),
+      p.printTable();
+    })
+    .catch((error) => {
+      console.error(
+        "\nError while fetching data from strapi server. Is it up?"
+      );
     });
-    p.addRow({
-      index: 2,
-      text: "strapi_process_virtual_memory_bytes",
-      value: convertBytesToMB(strapi_process_virtual_memory_bytes),
-    });
-    p.addRows([
-      {
-        index: 3,
-        text: "strapi_nodejs_external_memory_bytes",
-        value: convertBytesToMB(strapi_nodejs_external_memory_bytes),
-      },
-    ]);
-
-    p.printTable();
-  });
 }
 
 setInterval(() => {
@@ -77,4 +97,4 @@ setInterval(() => {
   console.log(`Project: ${projectName}`);
   console.log(`------ Memory ------`);
   getMetrics(urlMetrics);
-}, 2000);
+}, secondsUpdate);
